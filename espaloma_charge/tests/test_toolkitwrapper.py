ebf656cbda40d0828fd996d44751fb4e0095ab7c
Jeffrey Wagner <jwagnerjpl@gmail.com>
2022-11-08 14:16:28 -0800

    more extensive tests

--- espaloma_charge/tests/test_toolkitwrapper.py
+++ espaloma_charge/tests/test_toolkitwrapper.py
@@ -1,9 +1,20 @@
 from espaloma_charge.espaloma_wrapper import EspalomaChargeToolkitWrapper
+from openff.toolkit import Molecule
 
 
-def test_ethanol():
-    from openff.toolkit import Molecule
+def test_ethanol_molecule_api_w_registry():
     offmol = Molecule.from_smiles('CCO')
     offmol.assign_partial_charges('espaloma-am1bcc',
                                   toolkit_registry=EspalomaChargeToolkitWrapper())
-    print(offmol.partial_charges)
+    assert offmol.partial_charges != offmol.partial_charges * 0., offmol.partial_charges
+
+def test_ethanol_molecule_api_wo_registry():
+    offmol = Molecule.from_smiles('CCO')
+    offmol.assign_partial_charges('espaloma-am1bcc')
+    assert offmol.partial_charges != offmol.partial_charges * 0., offmol.partial_charges
+
+def test_ethanol_direct():
+    ectkw = EspalomaChargeToolkitWrapper()
+    offmol = Molecule.from_smiles('CCO')
+    ectkw.assign_partial_charges(offmol, 'espaloma-am1bcc')
+    assert offmol.partial_charges != offmol.partial_charges * 0., offmol.partial_charges
