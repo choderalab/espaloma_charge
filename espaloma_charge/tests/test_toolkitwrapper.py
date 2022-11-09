74f83b7e9670ff624f7c7acf0ca2506a75f2c9fd
Jeffrey Wagner <jwagnerjpl@gmail.com>
2022-11-08 16:39:41 -0800

    more tests

--- espaloma_charge/tests/test_toolkitwrapper.py
+++ espaloma_charge/tests/test_toolkitwrapper.py
@@ -1,20 +1,58 @@
 from espaloma_charge.espaloma_wrapper import EspalomaChargeToolkitWrapper
-from openff.toolkit import Molecule
+from openff.toolkit import ToolkitRegistry, ForceField, RDKitToolkitWrapper
+from openff.units import unit
+import pytest
+from openff.toolkit.utils.exceptions import ChargeMethodUnavailableError
+from openff.toolkit.tests.create_molecules import create_ethanol
 
-
-def test_ethanol_molecule_api_w_registry():
-    offmol = Molecule.from_smiles('CCO')
+def test_ethanol_molecule_api_w_wrapper():
+    ectkw = EspalomaChargeToolkitWrapper()
+    offmol = create_ethanol()
     offmol.assign_partial_charges('espaloma-am1bcc',
-                                  toolkit_registry=EspalomaChargeToolkitWrapper())
-    assert offmol.partial_charges != offmol.partial_charges * 0., offmol.partial_charges
+                                  toolkit_registry=ectkw)
+    # Ensure its not all 0s
+    assert (abs(offmol.partial_charges.value_in_unit(unit.elementary_charge)) > 0.01).any(), offmol.partial_charges
+
+def test_ethanol_molecule_api_w_explicit_registry():
+    ectkr = ToolkitRegistry([EspalomaChargeToolkitWrapper])
+    offmol = create_ethanol()
+    offmol.assign_partial_charges('espaloma-am1bcc',
+                                  toolkit_registry=ectkr)
+    # Ensure its not all 0s
+    assert (abs(offmol.partial_charges.value_in_unit(unit.elementary_charge)) > 0.01).any(), offmol.partial_charges
 
 def test_ethanol_molecule_api_wo_registry():
-    offmol = Molecule.from_smiles('CCO')
-    offmol.assign_partial_charges('espaloma-am1bcc')
-    assert offmol.partial_charges != offmol.partial_charges * 0., offmol.partial_charges
+    offmol = create_ethanol()
+    # This should raise a ChargeMethodUnavailableError since the global toolkit registry won't
+    # automatically add the espalomawrapper
+    with pytest.raises(ChargeMethodUnavailableError):
+        offmol.assign_partial_charges('espaloma-am1bcc')
+
 
 def test_ethanol_direct():
     ectkw = EspalomaChargeToolkitWrapper()
-    offmol = Molecule.from_smiles('CCO')
+    offmol = create_ethanol()
     ectkw.assign_partial_charges(offmol, 'espaloma-am1bcc')
-    assert offmol.partial_charges != offmol.partial_charges * 0., offmol.partial_charges
+    # Ensure its not all 0s
+    assert (abs(offmol.partial_charges.value_in_unit(unit.elementary_charge)) > 0.01).any(), offmol.partial_charges
+
+def test_create_system():
+    offmol = create_ethanol()
+    ff = ForceField('openff-2.0.0.offxml')
+    ff.deregister_parameter_handler('ToolkitAM1BCC')
+    ff.get_parameter_handler('ChargeIncrementModel', {'version': '0.3',
+                                                      'partial_charge_method': 'espaloma-am1bcc'})
+    sys = ff.create_openmm_system(offmol.to_topology(),
+                                  toolkit_registry=ToolkitRegistry([EspalomaChargeToolkitWrapper, RDKitToolkitWrapper]))
+
+def test_create_interchange():
+    offmol = create_ethanol()
+    ff = ForceField('openff-2.0.0.offxml')
+    ff.deregister_parameter_handler('ToolkitAM1BCC')
+    ff.get_parameter_handler('ChargeIncrementModel', {'version': '0.3',
+                                                      'partial_charge_method': 'espaloma-am1bcc'})
+    interchange = ff.create_interchange(offmol.to_topology(),
+                                        toolkit_registry=ToolkitRegistry([EspalomaChargeToolkitWrapper, RDKitToolkitWrapper]))
+
+
+# TODO: Tests for user-provided use_conformers, strict_n_conformers, normalize_partial_charges
