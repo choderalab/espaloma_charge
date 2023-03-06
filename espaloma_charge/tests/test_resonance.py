ff4cb34224a43ab3de65ec12501dfc468c47d8f7
Yuanqing Wang <wangyq+gh@wangyq.net>
2023-03-06 17:05:55 -0500

    rewrite test cases

--- espaloma_charge/tests/test_resonance.py
+++ espaloma_charge/tests/test_resonance.py
@@ -8,7 +8,7 @@ def test_cooh():
     mol = Chem.MolFromSmiles("[O-]C=O")
     mol = Chem.AddHs(mol)
     g = from_rdkit_mol(mol)
-    assert torch.unique(g.ndata["h0"][g.ndata["h0"][:, 2] == 1], dim=0).shape[0] == 1
+    assert torch.unique(g.ndata["h0"][g.ndata["type"] == 8], dim=0).shape[0] == 1
  
 def test_benzamidine():
     from rdkit import Chem
@@ -17,7 +17,12 @@ def test_benzamidine():
     mol = Chem.MolFromSmiles("C1=CC=C(C=C1)C(=[NH2+])N")
     mol = Chem.AddHs(mol)
     g = from_rdkit_mol(mol)
-    assert torch.unique(g.ndata["h0"][g.ndata["h0"][:, 1] == 1], dim=0).shape[0] == 1
+    assert torch.unique(g.ndata["h0"][g.ndata["type"] == 7], dim=0).shape[0] == 1
+
+    mol = Chem.MolFromSmiles("C1=CC=C(C=C1)C(=[N])N")
+    mol = Chem.AddHs(mol)
+    g = from_rdkit_mol(mol)
+    assert torch.unique(g.ndata["h0"][g.ndata["type"] == 7], dim=0).shape[0] == 2
         
 def test_guanidinium():
     import torch
@@ -26,7 +31,7 @@ def test_guanidinium():
     mol = Chem.MolFromSmiles("C(=[NH2+])(N)N")
     mol = Chem.AddHs(mol)
     g = from_rdkit_mol(mol)
-    assert torch.unique(g.ndata["h0"][g.ndata["h0"][:, 1] == 1], dim=0).shape[0] == 1
+    assert torch.unique(g.ndata["h0"][g.ndata["type"] == 7], dim=0).shape[0] == 1
  
 
 
